02ab1ad43a2614d5abea67e554bc1af4f1163dfb
AnnaMC00 <am.casariegoc00@gmail.com>
2024-05-04 13:01:08 -0300

    Contacts Api. GET part

--- Project1/app.js
+++ Project1/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 const routes = require("./routes/")
 const port = process.env.PORT || 3000;
 
-app.get('/', routes);
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and running on port ${port}`)
+        });
+    }
+})
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-});
+app.use('/', routes);
